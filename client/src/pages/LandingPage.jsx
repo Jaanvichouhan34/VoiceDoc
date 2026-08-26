245d12d1ba1ea937b50bb251e9d9a287875ba1b8
Jaanvi Chouhan <jaanvichouhan18805@gmail.com>
2026-08-27 01:43:28 +0530

    feat(landing): add smooth scroll navigation for section hash links

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import LandingNavbar from '../components/landing/LandingNavbar';
 import HeroSection from '../components/landing/HeroSection';
 import AboutSection from '../components/landing/AboutSection';
@@ -8,6 +9,22 @@ import ContactSection from '../components/landing/ContactSection';
 import Footer from '../components/landing/Footer';
 
 const LandingPage = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const id = location.hash.replace('#', '');
+      const element = document.getElementById(id);
+      if (element) {
+        setTimeout(() => {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }, 100);
+      }
+    } else {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  }, [location]);
+
   return (
     <div className="min-h-screen bg-[#0a0f1e] text-white selection:bg-primary/30">
       <LandingNavbar />
@@ -22,3 +39,4 @@ const LandingPage = () => {
 };
 
 export default LandingPage;
+
